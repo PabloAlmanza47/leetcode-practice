b8a7b164a7da26d969cc3c19aa0ff4f4de2b430c
Pablo Almanza <pabloalmanza3247@gmail.com>
2026-04-23 13:11:05 -0500

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hash_helper;
+
+        for (const auto& current_string : strs) {
+            string helper_string = current_string;
+            sort(helper_string.begin(), helper_string.end());
+            hash_helper[helper_string].push_back(current_string);
+        }
+        vector<vector<string>> result;
+        for (auto current_pair : hash_helper) {
+            result.push_back(current_pair.second);
+        }
+        return result;
+    }
+};
